2d87a5ba2cbb605528db8ad3f40a29ed6b34956b
Saeid Aliei <saeidaliei2019@gmail.com>
2022-07-19 02:48:06 +0430

    add stat method

--- 11-downloader/downloader.py
+++ 11-downloader/downloader.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import requests
-import threading
 import logging
 from pathlib import Path
 import re
@@ -11,6 +10,8 @@ import math
 import urllib
 import time
 from queue import Queue
+from threading import Thread
+import shutil
 # history
 # progress bar
 # download speed chart
@@ -150,15 +151,18 @@ class UrlParser:
 
 
 class Downloader(UrlParser):
-    def __init__(self, url, num_threads=4, **kwargs):
+    def __init__(self, url, num_threads=4, dldir=".", **kwargs):
         super().__init__(url, **kwargs)
         self._num_threads = num_threads
+        self._dldir = dldir
+        self.filepath = os.path.join(dldir, self.filename)
         # [(0, 9), (10, 19), (20, 28)]
-        self._byte_ranges = self.threads_byte_ranges()
+        self._byte_ranges = self.get_byte_ranges()
         # Queue({"chunk_id": 0, "chunk_range": (0, 10), "interrupted": False}, ...)
-        self.dlqueue = self.dlqueue()
+        self.dlqueue = self.get_dlqueue()
         self.start_time = None
-        
+        # filename pattern used for file chunks
+        self._chunk_filename = self.filename + ".part"
     
     @property
     def num_threads(self):
@@ -175,8 +179,22 @@ class Downloader(UrlParser):
     @num_threads.deleter
     def num_threads(self):
         raise AttributeError("num_threads can't be deleted. Set to 1 to use only one thread.")
+    
+    @property
+    def dldir(self):
+        return self._dldir
+    
+    @dldir.setter
+    def dldir(self, dldir):
+        disk_total_size, disk_used_size, disk_available_size = shutil.disk_usage(dldir)
+        if disk_available_size < self.filesize:
+            raise ValueError("Provided download directory doesn't have enough storage space.")
+        elif not os.path.isdir(dldir):
+            raise FileNotFoundError("The download directory doesn't exists.")
+        else:
+            self._dldir = dldir
 
-    def threads_byte_ranges(self):
+    def get_byte_ranges(self):
         """Bytes range specific to a thread which will be downloaded by that thread."""
         thread_ranges = []
         chunk_start = 0
@@ -195,22 +213,72 @@ class Downloader(UrlParser):
 
         return thread_ranges
 
-    def dlqueue(self):
+    def get_dlqueue(self):
         """Queue the chunks for threads to pick from."""
         # {"0": {"range": (0, 10), "interrupted": False}, }
         dlqueue = Queue(maxsize=0)
         for chunk_id, chunk_range in enumerate(self._byte_ranges):
             item = {"chunk_id": chunk_id, "chunk_range": chunk_range, "interrupted": False}
             dlqueue.put(item)
+        return dlqueue
 
     def timestamp(self):
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         return now
 
     def download(self):
+        """Main driver function for starting downloader threads."""
         self.start_time = self.timestamp()
         if self.resumable:
             logging.debug("Starting download in a resumable mode.")
+            for _ in range(self.num_threads):
+                thread = Thread(target=self.download_chunk)
+                thread.daemon = True
+                thread.start()
+            logging.debug("Download threads have been started.")
+       
+        self.log_dlstat()
+        # wait until all threads are done
+        self.dlqueue.join()
+
+        logging.debug("Merging file chunks into single file.")
+
+    
+    def log_dlstat(self):
+        while not self.isfinished():
+            dlstat = self.dlstat()
+            stat_str = ""
+            for stat in dlstat:
+                stat_str += "Part {}: {}%\t".format(*stat)
+            logging.info("Download status: {}".format(stat_str))
+            time.sleep(5)
+        if self.isfinished():
+            logging.info("Download complete.")
+
+    def dlstat(self):
+        """Get download percentage for each thread."""
+        # [(1, 98), (2, 65), ...]
+        dlstat = []
+        for thread_idx in range(self.num_threads):
+            chunk_path = os.path.join(self.dldir, self._chunk_filename+str(thread_idx))
+            if os.path.isfile(chunk_path):
+                chunk_dlsize = os.stat(chunk_path).st_size
+                chunk_dlperc = round((chunk_dlsize / (self.filesize/self.num_threads)) * 100, 2)
+                dlstat.append((thread_idx, chunk_dlperc))
+            else:
+                dlstat.append((thread_idx, 0.00))
+        return dlstat
+
+    def isfinished(self):
+        dlstat = dlstat()
+        sec_elm = lambda x: x[1]
+        percentages = [sec_elm(stat) for stat in dlstat]
+        if all(percent == 100.00 for percent in percentages):
+            return True
+        else:
+            return False
+
+
 
 
 
