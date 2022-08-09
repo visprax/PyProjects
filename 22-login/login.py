3b4823fa80d01cbb02abadd16c1161aab422f7fc
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-09 16:16:52 +0430

    validate config file

--- 22-login/login.py
+++ 22-login/login.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import configparser
 from getpass import getpass
 
@@ -10,6 +11,14 @@ def isvalid(username, password):
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("login.conf")
+
+    store_type = config["default"]["store_type"]
+    if store_type not in ["db", "json", "yaml"]:
+        raise SystemExit(f"'store_type': {store_type} in config file not supported.")
+    store_path = config["default"]["store_path"]
+    if not os.path.exists(store_path):
+        raise SystemExit(f"'store_path': {store_path} in config file doesn't exist.")
+    store_name = config["default"]["store_name"] + '.' + store_type
 
     username = input("Enter your username:> ")
     password = getpass("Enter your password:> ")
