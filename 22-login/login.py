e634c0e254d84eecdd92eb9aff82e07b70a98349
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-09 16:50:21 +0430

    add supported commands

--- 22-login/login.py
+++ 22-login/login.py
@@ -4,7 +4,15 @@ import os
 import configparser
 from getpass import getpass
 
-def isvalid(username, password):
+SUPPORTED_STORE_TYPES = ["db", "json", "yaml"]
+SUPPORTED_COMMANDS = ["/login", "/change", "/register"]
+
+def read_file(filepath, filetype):
+    if filetype == "yaml":
+
+
+
+def is_valid(username, password):
     return True
 
 
@@ -13,17 +21,40 @@ if __name__ == "__main__":
     config.read("login.conf")
 
     store_type = config["default"]["store_type"]
-    if store_type not in ["db", "json", "yaml"]:
+    if store_type not in SUPPORTED_STORE_TYPES:
         raise SystemExit(f"'store_type': {store_type} in config file not supported.")
     store_path = config["default"]["store_path"]
     if not os.path.exists(store_path):
         raise SystemExit(f"'store_path': {store_path} in config file doesn't exist.")
     store_name = config["default"]["store_name"] + '.' + store_type
-
-    username = input("Enter your username:> ")
-    password = getpass("Enter your password:> ")
     
-    if isvalid(username, getpass):
+    filepath = store_path + store_name
+    if os.path.isfile(filepath):
+        pfile_exists = True
+    else:
+        pfile_exists = False
+    
+    print("Command could be one of '/login', '/change', or '/register'")
+    command = input("command:> ")
+    if command not in SUPPORTED_COMMANDS:
+        raise SystemExit(f"'command': {command} is not supported.")
+    
+    if command == "/login" and not pfile_exists:
+        raise SystemExit(f"'password file': {filepath} doesn't exists, can't login.")
+
+    if command == "/login" and pfile_exists:
+        passwords = read_file(filepath, store_type)
+        username = input("username:> ")
+        password = getpass("password:> ")
+    
+    if command == "/change":
+        pass
+
+    if command == "/register":
+        pass
+    
+    
+    if is_valid(username, getpass):
         print("Login successful.")
     else:
         print("Access denied.")
