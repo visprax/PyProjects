90489cf66ba269dff22b02c2df2f4bd1c62938fd
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-10 21:43:24 +0430

    separate utility function

--- 22-login/login.py
+++ 22-login/login.py
@@ -1,163 +1,17 @@
 #!/usr/bin/env python3
 
 import os
-import re
-import json
-import yaml
-import sqlite3
 import logging
-import hashlib
 import argparse
 from getpass import getpass
 
 from config_parser import validate_config
+from utils import login_user, change_password, register_user
 
 # TODO: get username and email from command line
 # TODO: email notifications
 
-def get_hash(string):
-    return hashlib.sha256(string.encode("utf-8")).hexdigest()
-
-def register_user(username, password, store_type, passwords_path):
-    if store_type == "yaml":
-        logger.debug(f"opening passwords file: '{passwords_path}'")
-        with open(passwords_path, 'r+') as yamlfile:
-            data = yaml.safe_load(yamlfile)
-            for entry in data:
-                if entry["username"] == username:
-                    logger.debug(f"username: {username} already registered")
-                    print("Username exists.")
-                    return False
-            entry = {"username": username, "password_hash": get_hash(password)}
-            data.append(entry)
-            logger.debug("writing updated data to: '{passwords_path}'")
-            yaml.safe_dump(data, yamlfile)
-        return True
-
-    elif store_type == "json":
-        logger.debug(f"opening passwords file: '{passwords_path}'")
-        with open(passwords_path, 'r+') as jsonfile:
-            data = json.load(jsonfile)
-            for entry in data:
-                if entry["username"] == username:
-                    logger.debug(f"username: {username} already registered")
-                    print("Username exists.")
-                    return False
-            entry = {"username": username, "password_hash": get_hash(password)}
-            data.append(entry)
-            logger.debug("writing updated data to: '{passwords_path}'")
-            json.dump(data, jsonfile)
-        return True
-
-    else:
-        just_created_database = False
-        if not os.path.isfile(passwords_path):
-            logger.debug(f"passwords database: '{passwords_path}' doesn't exist, creating")
-            conn = sqlite3.connect(passwords_path)
-            curs = conn.cursor()
-            query = "CREATE TABLE users (username VARCHAR, password_hash VARCHAR)"
-            conn.commit()
-            curs.close()
-            conn.close()
-            just_created_database = True
-
-        logger.debug(f"connecting to passwords database: '{passwords_path}'")
-        conn = sqlite3.connect(passwords_path)
-        curs = conn.cursor()
-        # check if username is in database if we didn't just created it
-        if just_created_database:
-            logger.debug(f"inserting user to the databse")
-            query = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-            curs.execute(query, (username, get_hash(password)))
-            conn.commit()
-        else:
-            logger.debug(f"checking availability of the username")
-            query = "SELECT * FROM users WHERE username = ?"
-            result = curs.execute(query, (username,)).fetchone()
-            conn.commit()
-            if result:
-                logger.debug(f"username: {username} already registered")
-                print("Username exists.")
-                return False
-            else:
-                query = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-                curs.execute(query, (username, get_hash(password)))
-                conn.commit()
-        curs.close()
-        conn.close()
-        return True
-
-def change_password(username, password, store_type, passwords_path):
-    if store_type == "yaml":
-        logger.debug(f"opening yaml passwords file: '{passwords_path}'")
-        with open(passwords_path, "r+") as yamlfile:
-            data = yaml.safe_load(yamlfile)
-            for entry in data:
-                if entry["username"] == username:
-                    entry.update({"password_hash": get_hash(password)})
-                    break
-            yaml.safe_dump(data, yamlfile)
-
-    elif store_type == "json":
-        logger.debug(f"opening json passwords file: '{passwords_path}'")
-        with open(passwords_path, "r+") as jsonfile:
-            data = json.load(jsonfile)
-            for entry in data:
-                if entry["username"] == username:
-                    entry.update({"password_hash": get_hash(password)})
-            json.dump(data, jsonfile)
-
-    else:
-        logger.debug(f"connection to password database: '{passwords_path}'")
-        conn = sqlite3.connect(passwords_path)
-        curs = conn.cursor()
-        query = "UPDATE users SET password_hash = ? WHERE username = ?"
-        curs.execute(query, (username, get_hash(password)))
-        logger.debug(f"executed query on database.")
-        conn.commit()
-        curs.close()
-        conn.close()
-
-
-def is_valid(username, password, store_type, passwords_path):
-    if not os.path.isfile(passwords_path):
-        logger.critical(f"passwords database file '{passwords_path}' doesn't exist")
-        raise SystemExit()
-
-    logger.info(f"checking validity of username: {username} against provided password")
-    if store_type in ["yaml", "json"]:
-        if store_type == "yaml":
-            logger.debug(f"reading yaml passwords file: '{passwords_path}'")
-            with open(passwords_path, 'r') as yamlfile:
-                data = yaml.safe_load(yamlfile)
-        else:
-            logger.debug(f"reading json passwords file: '{passwords_path}'")
-            with open(passwords_path, 'r') as jsonfile:
-                data = json.load(jsonfile)
-        generator = (item for item in data if item["username"]==username)
-        # if the username is in data return that entry else return false
-        entry = next(generator, False)
-        if entry:
-            if entry["password_hash"] == get_hash(password):
-                return True
-            else:
-                return False
-        return entry
-    
-    else:
-        logger.debug(f"querying passwords database: '{passwords_path}'")
-        conn = sqlite3.connect(passwords_path)
-        curs = conn.cursor()
-        query = "SELECT * FROM users WHERE username = ? AND password_hash = ?"
-        entry = curs.execute(query, (username, get_hash(password))).fetchone()
-        logger.debug(f"executed query on database.")
-        conn.commit()
-        curs.close()
-        conn.close()
-        return entry is not None
-
-
-if __name__ == "__main__":
+def main():
     logging.basicConfig(format='%(asctime)s  %(name)s  %(levelname)s: %(message)s')
     logger = logging.getLogger("xlogin")
 
@@ -198,7 +52,7 @@ if __name__ == "__main__":
         password = getpass("password:> ")
 
         logger.info(f"attempting to login with username: '{username}'")
-        if is_valid(username, password, config["store_type"], config["passwords_path"]):
+        if login_user(username, password, config["store_type"], config["passwords_path"]):
             print("Login successful.")
         else:
             print("Access denied.")
@@ -206,7 +60,7 @@ if __name__ == "__main__":
     elif args.command == "change":
         username = input("username:> ")
         password = getpass("old password:> ")
-        if is_valid(username, password, config["store_type"], config["passwords_path"]):
+        if login_user(username, password, config["store_type"], config["passwords_path"]):
             logger.info(f"attempting to change password for username: '{username}'")
             # 3 tries for changing password in case the two passwords don't match
             tries = 0
@@ -249,3 +103,6 @@ if __name__ == "__main__":
             print("Try again later.")
             raise SystemExit()
 
+
+if __name__ == "__main__":
+    main()
