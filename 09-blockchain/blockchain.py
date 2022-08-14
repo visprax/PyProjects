499549c2fdf758b8d4bdaef482e2eb41449a7303
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-14 11:08:31 +0430

    add block and transactions methods

---
+++ 09-blockchain/blockchain.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""A rudimentary blockchain implementation."""
+
+import os
+import time
+import logging
+import hashlib
+
+
+class Blockchain:
+    """A rudimentary blockchain implementation.
+    
+    TODO: a simple explanation of a blockchain.
+
+    """
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+
+        # create the genesis block
+        self.new_block(proof=100, previous_hash=1)
+
+    def new_block(self, proof, previous_hash=None):
+        """Create a new block in the blockchain.
+
+        Args:
+            proof (int): The proof given by the proof of work algorithm.
+            previous_hash(int, optional): Hash of the previous block. Defaults to None.
+
+        Returns:
+            dict: The new block.
+    
+        """
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time.time(),
+            "transactions": self.transactions,
+            "proof": proof,
+            "previous_hash": previous_hash if previous_hash else self.hash(self.chain[-1])
+                }
+        self.chain.append(block)
+        # reset the transactions for the new block
+        self.transactions = []
+        return block
+    
+    def new_transaction(self, sender, recipient, amount):
+        """Create a new transaction that will go into the next mined block.
+        
+        Args:
+            sender (str): Address of the sender.
+            recipient (str): Address of the recipient.
+            amount (int): The amount of transaction.
+
+        Returns:
+            int: The index of the block that will hold this transaction.
+        
+        """
+        transaction = {
+                "sender": sender,
+                "recipient": recipient,
+                "amount": amount
+                }
+        self.transactions.append(transaction)
+        # return the index of block which the transaction will be added to
+        return self.last_block["index"] + 1
+    
+    @property
+    def last_block(self):
+        """Get the last block in the chain."""
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """Create the SHA256 hash of a block.
+
+        Note:
+            The block should be an ORDERED dictionary, Or the resulting hashes
+                maybe inconsistent.
+
+        Args:
+            block (dict): The block to be hashed.
+
+        Returns:
+            str: The hash of the block.
+
+        """
+        block_bytes = json.dumps(block, sort_keys=True).encode()
+        block_hash = hashlib.sha256(block_bytes).hexdigest()
+        return block_hash
+
+
+
