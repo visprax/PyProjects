6f21f8152c9e800ffe111e984960780a6bb6bdd9
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-17 02:49:56 +0430

    load configs from config module

--- 10-dashboard/stocker.py
+++ 10-dashboard/stocker.py
@@ -6,6 +6,7 @@ import argparse
 import configparser
 
 from utils.db import questdb_create_table
+from utils.config_parser import read_config
 
 logger = logging.getLogger("stocker")
 
@@ -15,25 +16,32 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(prog="stocker", description="Real-time stocks and cryptocurrency charts")
     argparser.add_argument("--conf", help="path to config file")
     argparser.add_argument("-v", help="print logging message", action="count")
+    args = argparser.parse_args()
+
     if not args.v:
-        logging.disable()
+        log_level = None
     elif args.v == 1:
         log_level = "ERROR"
     elif args.v == 2:
         leg_level == "INFO"
     else:
         log_level = "DEBUG"
-    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-    for logger in loggers:
-        logger.setLevel(log_level)
+
+    if log_level:
+        loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+        for logger in loggers:
+            logger.setLevel(log_level)
+    else:
+        logging.disable()
     
     logger.info("reading config file")
     config = configparser.ConfigParser()
     if args.conf:
-        conf.read(args.conf)
+        filepath = args.conf
     else:
-        conf.read("app.conf")
+        filepath = "app.conf"
+
+    params = read_config(filepath)
 
     result = questdb_create_table()
 
-    print(result)
