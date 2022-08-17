9c9a830beb4cb820be12a3f9a699aebff53b2660
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-17 16:42:24 +0430

    change to OO design

--- 10-dashboard/utils/db.py
+++ 10-dashboard/utils/db.py
@@ -1,45 +1,49 @@
 import logging
 import psycopg2 as pg
 
-logger = logging.getLogger("db")
+logger = logging.getLogger("qdb")
 
-def questdb_create_table(params):
-    logger.info("creating quotes tables in database if it doesn't already exists")
-    connection = None
-    cursor = None
-    query = "CREATE TABLE IF NOT EXISTS "\
-                "quotes("\
-                    "stock_symbol SYMBOL CAPACITY 5 CACHE INDEX, "\
-                    "current_price DOUBLE, "\
-                    "high_price DOUBLE, "\
-                    "low_price DOUBLE, "\
-                    "open_price DOUBLE, "\
-                    "percent_change DOUBLE, "\
-                    "trade_time TIMESTAMP, "\
-                    "insert_time TIMESTAMP"\
-                ") "\
-                "timestamp(insert_time) "\
-            "PARTITION BY DAY;"
-    try:
-        connection = pg.connect(
-                user     = params["database"]["user"],
-                password = params["database"]["password"],
-                host     = params["database"]["host"],
-                port     = params["database"]["port"],
-                database = params["database"]["database"]
-                )
-        cursor = connection.cursor()
-        result = cursor.execute(query)
-        connection.commit()
-    except Exception as err:
-        logger.error("error occured during creating quotes table in database: {err}", exc_info=True)
-        raise SystemExit()
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-            logger.info("connection to database closed")
+class Qdb:
+    def __init__(self, params):
+        self.params = params
 
-    return result
+    def create_table(self):
+        logger.info("creating quotes tables in database if it doesn't already exists")
+        connection = None
+        cursor = None
+        query = "CREATE TABLE IF NOT EXISTS "\
+                    "quotes("\
+                        "stock_symbol SYMBOL CAPACITY 5 CACHE INDEX, "\
+                        "current_price DOUBLE, "\
+                        "high_price DOUBLE, "\
+                        "low_price DOUBLE, "\
+                        "open_price DOUBLE, "\
+                        "percent_change DOUBLE, "\
+                        "trade_time TIMESTAMP, "\
+                        "insert_time TIMESTAMP"\
+                    ") "\
+                    "timestamp(insert_time) "\
+                "PARTITION BY DAY;"
+        try:
+            connection = pg.connect(
+                    user     = self.params["database"]["user"],
+                    password = self.params["database"]["password"],
+                    host     = self.params["database"]["host"],
+                    port     = self.params["database"]["port"],
+                    database = self.params["database"]["database"]
+                    )
+            cursor = connection.cursor()
+            result = cursor.execute(query)
+            connection.commit()
+        except Exception as err:
+            logger.error("error occured during creating quotes table in database: {err}", exc_info=True)
+            raise SystemExit()
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+                logger.info("connection to database closed")
+
+        return result
 
