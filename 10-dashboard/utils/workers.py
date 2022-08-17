f93568196c38ff002c2ad054bf7d0df860dcbf69
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-17 16:42:44 +0430

    change to OO design and fix params passing issue

--- 10-dashboard/utils/workers.py
+++ 10-dashboard/utils/workers.py
@@ -1,12 +1,16 @@
+import logging
 from celery import Celery
 from finnhub import Client
 
-from config_parser import read_config
+logger = logging.getLogger("workers")
 
+class Workers:
+    def __init__(self, params):
+        self.params = params
+        self.celery = Celery(broker=params["celery"]["broker"])
+        self.client = Client(api_key=params["finnhub"]["key"])
 
-celery = Celery(broker=params["celery"]["broker"])
-client = Client(api_key=params["finnhub"]["key"])
+    def setup_periodic_tasks(self):
+        """Periodic tasks setup for each symbol."""
+        pass
 
-def setup_periodic_tasks(sender, **kwargs):
-    """Periodic tasks setup for each symbol."""
-    sender.add_periodic_task()
