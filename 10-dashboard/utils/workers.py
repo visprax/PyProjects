46c18a02eadfd132abba3f96fcf175fe6e2233b1
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-23 16:08:28 +0430

    add celery tasks and periodic tasks

--- 10-dashboard/utils/workers.py
+++ 10-dashboard/utils/workers.py
@@ -4,28 +4,31 @@ from finnhub import Client
 
 from utils.db import Qdb
 
-logger = logging.getLogger("workers")
+logger = logging.getLogger("worker")
 
-class Workers:
+class Worker:
     def __init__(self, params):
+        logger.info("setting up worker")
         self.params = params
-        self.celery = Celery(broker=params["celery"]["broker"])
-        self.client = Client(api_key=params["finnhub"]["key"])
         self.db = Qdb(params)
-        logger.info("workers are setup.")
 
-    @self.celery.on_after_configure.connect
-    def setup_periodic_tasks(self, sender):
-        """Periodic tasks setup for each symbol."""
-        logger.info("setting up periodic tasks for each symbol")
-        freq = self.params["finnhub"]["frequency"]
-        for symbol in self.params["finnhub"]["symbols"]:
-            sender.add_periodic_task(freq, self.fetch.s(symbol))
-    
-    @self.celery.task
-    def fetch(self, symbol):
-        """Fetch symbol quotes from Finnhub and insert into database."""
-        # for an explanation on the quote structure, 
-        # see: https://finnhub.io/docs/api/quote
-        quote = self.client.quote(symbol)
-        self.db.insert_quote(symbol, quote)
+        logger.info("setting up Celery")
+        celery = Celery(broker=params["celery"]["broker"])
+        logger.info("setting up Finnhub client")
+        client = Client(api_key=params["finnhub"]["key"])
+
+        @celery.on_after_configure.connect
+        def setup_periodic_tasks(sender, **kwargs):
+            """Periodic tasks setup for each symbol."""
+            logger.info("setting up periodic tasks for each symbol")
+            freq = self.params["finnhub"]["frequency"]
+            for symbol in self.params["finnhub"]["symbols"]:
+                sender.add_periodic_task(freq, fetch.s(symbol))
+
+        @celery.task
+        def fetch(symbol):
+            """Fetch symbol quotes from Finnhub and insert into database."""
+            # for an explanation on the quote structure, 
+            # see: https://finnhub.io/docs/api/quote
+            quote = client.quote(symbol)
+            self.db.insert_quote(symbol, quote)
