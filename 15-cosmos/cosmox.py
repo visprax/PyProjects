0e931ca409e112056f23508c22e0a0f2671f2480
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-27 01:29:11 +0430

    add Cosmology class

---
+++ 15-cosmos/cosmox.py
@@ -0,0 +1,39 @@
+import math
+from dataclasses import dataclass
+
+@dataclass
+class Cosomology:
+    H0: float
+    OmegaM: float
+    OmefaL: float
+
+    @property
+    def OmegaK(self):
+        """Dimensionless curvature density of the Universe.
+        
+        $\OmegaM + \OmegaL + \OmegaK = 1$,
+        $\Omega_M$ is matter density parameter and 
+        $\Omega_L$ is density paramter of Dark Energy.
+        
+
+        Returns:
+            OmegaK(float): Density paramter resulting from curvatur.
+        """
+        OmegaK = 1 - self.OmegaM - self.OmegaL
+        return OmegaK
+
+    @property
+    def G(self):
+        """Newtonian gravitational constant."""
+        pass
+    
+    def adot(self):
+        """Time derivative of dimensionless expansion factor.
+
+        Friedmann equation: H^2 / H_0^2 = \Omega_Lambda + \Omega_M / a^3 + \Omega_k / a^2
+        
+        Returns:
+            \dot{a}(float): Time derivate of dimensionless expansion factor.
+        """
+        adot = self.H0 * a * math.sqrt(self.OmegaL + (self.OmegaM / a**3) + (self.OmegaK / a**2))
+        return adot
