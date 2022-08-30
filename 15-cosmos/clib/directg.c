2a2a68c542edbd1f060d5df4797a712e8ab35a7e
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-31 00:38:51 +0430

    add particle struct

--- 15-cosmos/clib/directg.c
+++ 15-cosmos/clib/directg.c
@@ -1,6 +1,32 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-int square(int i)
+#define NUM_PARTICLES 100
+#define G 1
+#define SOFTENING 0.05
+#define RAND_POS() (double) rand() / RAND_MAX - 0.5
+
+typedef struct 
 {
-    return i*i;
+    double position[3];
+    double velocity[3];
+    double mass;
+} Particle;
+
+Particle init()
+{
+    struct particle;
+
+    particle.position[0] = RAND_POS();
+    particle.position[1] = RAND_POS();
+    particle.position[2] = RAND_POS();
+
+    particle.velocity[0] = 0.0;
+    particle.velocity[1] = 0.0;
+    particle.velocity[2] = 0.0;
+
+    particle.mass = 1.0;
+
+    return particle;
 }
