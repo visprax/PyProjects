cac8ec439093eb27375ab8a1e26a9c5bbdb0b666
Saeid Aliei <saeidaliei2019@gmail.com>
2022-08-31 22:18:39 +0430

    add compute_forces function and some tests

--- 15-cosmos/clib/directg.c
+++ 15-cosmos/clib/directg.c
@@ -3,9 +3,8 @@
 #include <math.h>
 
 #define NUM_PARTICLES 100
-#define G 1
-#define SOFTENING 0.05
-#define RAND_POS() (double) rand() / RAND_MAX - 0.5
+#define GRAVITATIONAL_CONSTANT 1.0
+#define SOFTENING_FACTOR 0.05
 
 typedef struct 
 {
@@ -14,19 +13,103 @@ typedef struct
     double mass;
 } Particle;
 
-Particle init()
+/*
+ * Initialize particles, set position in each direction in Cartesian 
+ * coordinate to a random value less than 1, and set velocity of each
+ * particle to zero in all directions.
+ */
+Particle* init_particles(size_t size)
 {
-    struct particle;
+    Particle* particles = malloc(size * sizeof(Particle));
+    if (particles == NULL)
+    {
+        fprintf(stderr, "Error during allocating memory storage for particles.");
+        exit(EXIT_FAILURE);
+    }
 
-    particle.position[0] = RAND_POS();
-    particle.position[1] = RAND_POS();
-    particle.position[2] = RAND_POS();
+    for (int i = 0; i < size; i++)
+    {
+        particles[i].position[0] = (double) rand() / RAND_MAX  - 0.5;
+        particles[i].position[1] = (double) rand() / RAND_MAX  - 0.5;
+        particles[i].position[2] = (double) rand() / RAND_MAX  - 0.5;
 
-    particle.velocity[0] = 0.0;
-    particle.velocity[1] = 0.0;
-    particle.velocity[2] = 0.0;
+        particles[i].velocity[0] = 0.0;
+        particles[i].velocity[1] = 0.0;
+        particles[i].velocity[2] = 0.0;
 
-    particle.mass = 1.0;
+        particles[i].mass = 1.0;
+    }
 
-    return particle;
+    return particles;
+}
+
+/*
+ * Initialize forces for all particles in each directions to zero.
+ */
+double* init_forces(size_t num_particles)
+{
+    double* forces = malloc(3 * num_particles * sizeof(double));
+    if (forces == NULL)
+    {
+        fprintf(stderr, "Error during allocating memory storage for forces.");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 3 * num_particles; i++) forces[i] = 0.0;
+
+    return forces;
+}
+
+/*
+ * Calculate force on particle i due to particle j 
+ * in each Cartesian coordinate axis, (x, y, z) is
+ * the vector pointing from particle i to j which is
+ * the direction of the gravitational force. 
+ */
+double* compute_forces(Particle* particles, double* forces, size_t num_particles)
+{
+    double G  = GRAVITATIONAL_CONSTANT;
+    double Rs = SOFTENING_FACTOR;
+
+    for (int i = 0; i < num_particles; i++)
+        for (int j = 0; j < num_particles; j++)
+        {
+            double dx = particles[j].position[0] - particles[i].position[0];
+            double dy = particles[j].position[1] - particles[i].position[1];
+            double dz = particles[j].position[1] - particles[i].position[2];
+
+            double R2 = dx*dx + dy*dy + dz*dz;
+            /* 1 / (R2 + Rs)^(3/2) */
+            double Ri = pow(R2 + Rs, -1.5);
+
+            double M2 = particles[j].mass * particles[i].mass;
+
+            double Fx = G * M2 * Ri * dx;
+            double Fy = G * M2 * Ri * dy;
+            double Fz = G * M2 * Ri * dz;
+
+            forces[3 * i + 0] += Fx;
+            forces[3 * i + 1] += Fy;
+            forces[3 * i + 2] += Fz;
+        }
+
+    return forces;
+}
+
+void direct_nbody()
+{
+
+}
+
+int main()
+{
+    size_t num_particles = NUM_PARTICLES;
+
+    Particle* particles = init_particles(num_particles);
+    double* forces = init_forces(num_particles);
+
+    printf("particle 1's mass: %f\n", particles[0].mass);
+    printf("particle 2's x: %f\n", particles[1].position[0]);
+    printf("particle 3's vy: %f\n", particles[2].velocity[1]);
+    printf("particle 4's fz: %f\n", forces[3 * 3 + 2]);
 }
