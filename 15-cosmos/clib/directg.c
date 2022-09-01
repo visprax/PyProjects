bcda50d58f9fa0cf4bafef0c27e28d6f4d33b23d
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-01 18:06:02 +0430

    test total energy in center of mass coords

--- 15-cosmos/clib/directg.c
+++ 15-cosmos/clib/directg.c
@@ -24,9 +24,9 @@ Particle* init_particles(size_t num_particles)
         particles[i].position[1] = (double) rand() / RAND_MAX  - 0.5;
         particles[i].position[2] = (double) rand() / RAND_MAX  - 0.5;
 
-        particles[i].velocity[0] = 0.0;
-        particles[i].velocity[1] = 0.0;
-        particles[i].velocity[2] = 0.0;
+        particles[i].velocity[0] = (double) rand() / RAND_MAX  - 0.5;
+        particles[i].velocity[1] = (double) rand() / RAND_MAX  - 0.5;
+        particles[i].velocity[2] = (double) rand() / RAND_MAX  - 0.5;
 
         particles[i].mass = 1.0;
     }
@@ -108,25 +108,26 @@ void compute_forces(Particle* particles, double* forces, size_t num_particles)
         forces[3 * i + 2] = 0.0;
 
         for (int j = 0; j < num_particles; j++)
-        {
-            double dx = particles[j].position[0] - particles[i].position[0];
-            double dy = particles[j].position[1] - particles[i].position[1];
-            double dz = particles[j].position[1] - particles[i].position[2];
+            if ( j != i )
+            {
+                double dx = particles[j].position[0] - particles[i].position[0];
+                double dy = particles[j].position[1] - particles[i].position[1];
+                double dz = particles[j].position[2] - particles[i].position[2];
 
-            double R2 = dx*dx + dy*dy + dz*dz;
-            // 1 / (R2 + Rs)^(3/2)
-            double Ri = pow(R2 + Rs*Rs, -1.5);
+                double R2 = dx*dx + dy*dy + dz*dz;
+                // 1 / (R2 + Rs)^(3/2)
+                double Ri = pow(R2 + Rs*Rs, -1.5);
 
-            double M2 = particles[j].mass * particles[i].mass;
+                double M2 = particles[j].mass * particles[i].mass;
 
-            double Fx = G * M2 * Ri * dx;
-            double Fy = G * M2 * Ri * dy;
-            double Fz = G * M2 * Ri * dz;
+                double Fx = G * M2 * Ri * dx;
+                double Fy = G * M2 * Ri * dy;
+                double Fz = G * M2 * Ri * dz;
 
-            forces[3 * i + 0] += Fx;
-            forces[3 * i + 1] += Fy;
-            forces[3 * i + 2] += Fz;
-        }
+                forces[3 * i + 0] += Fx;
+                forces[3 * i + 1] += Fy;
+                forces[3 * i + 2] += Fz;
+            }
     }
 }
 
@@ -141,19 +142,57 @@ void directg()
     Particle* particles = init_particles(num_particles);
     double* forces = init_forces(num_particles);
 
+    if (COM_COORDS)
+    {
+        double tot_mass = 0.0;
+        for (int i = 0; i < num_particles; i++)
+        {
+            tot_mass += particles[i].mass;
+        }
+        double mean_mass = tot_mass / num_particles;
+
+        for (int i = 0; i < num_particles; i++)
+        {
+            double MVx = 0.0;
+            double MVy = 0.0;
+            double MVz = 0.0;
+            for(int j = 0; j < num_particles; j++)
+            {
+                MVx += particles[j].mass * particles[j].velocity[0];
+                MVy += particles[j].mass * particles[j].velocity[1];
+                MVz += particles[j].mass * particles[j].velocity[2];
+            }
+            double mean_MVx = MVx / num_particles;
+            double mean_MVy = MVy / num_particles;
+            double mean_MVz = MVz / num_particles;
+            
+            particles[i].velocity[0] -= mean_MVx / mean_mass;
+            particles[i].velocity[1] -= mean_MVy / mean_mass;
+            particles[i].velocity[2] -= mean_MVz / mean_mass;
+        }
+    }
+
     while (t < 1.0)
     {
         compute_forces(particles, forces, num_particles);
+
+        double KE = kinetic_energy(particles, num_particles);
+        double PE = potential_energy(particles, num_particles);
+        fprintf(stdout, "\nKinetic Energy: %f", KE);
+        fprintf(stdout, "\nPotential Energy: %f", PE);
+        fprintf(stdout, "\nTotal Energy: %f", KE+PE);
 
         for (int i = 0; i < num_particles; i++)
         {
             Particle* particle = &particles[i];
             double* force = &forces[3 * i];
 
+            // Half kick
             particle->velocity[0] += 0.5 * (force[0] / particle->mass) * dt;
             particle->velocity[1] += 0.5 * (force[1] / particle->mass) * dt;
             particle->velocity[2] += 0.5 * (force[2] / particle->mass) * dt;
-
+            
+            // Drif
             particle->position[0] += particle->velocity[0] * dt;
             particle->position[1] += particle->velocity[1] * dt;
             particle->position[2] += particle->velocity[2] * dt;
