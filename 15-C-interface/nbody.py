9faf7442224c5f785999559dc55a9ebe6f31d01d
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-03 20:44:42 +0430

    parse arguments and initiate loggers

--- 15-C-interface/nbody.py
+++ 15-C-interface/nbody.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 
-import ctypes
+import logging
+import argparse
 
-clib = ctypes.CDLL("clib/arrays.so")
+from utils.wrapper import Params, Particle, Solver
 
-print(clib.square(10));
+
+logger = logging.getLogger("nbody")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s  %(name)s  %(levelname)s: %(message)s')
+
+    argparser = argparse.ArgumentParser(prog="nbody", \
+            description="Solve gravitational NBody problem using a Leap-frog integration scheme.")
+    argparser.add_argument("-v", help="control verbosity of the output including debugging messages", action="count")
+    args = argparser.parse_args()
+
+    if not args.v:
+        log_level = None
+    elif args.v == 1:
+        log_level = "ERROR"
+    elif args.v == 2:
+        log_level = "INFO"
+    else:
+        log_level = "DEBUG"
+
+    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    if not log_level:
+        logging.disable()
+    else:
+        for logger in loggers:
+            logger.setLevel(log_level)
+
