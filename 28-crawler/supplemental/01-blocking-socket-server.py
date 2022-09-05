ce4a1f497fabc53620e521ded60a109ca8c8c29e
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-06 00:28:31 +0430

    add a blocking server

--- 28-crawler/supplemental/01-blocking-socket-server.py
+++ 28-crawler/supplemental/01-blocking-socket-server.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 
-"""A simple example of blocking calls with sockets."""
+"""A simple server with blocking sockets.
+    The server responds with the reversed text a client sends."""
 
 import time
 import socket
 
-start = time.perf_counter()
+def server():
+    # By default the socket is blocking
+    server = socket.socket()
+    
+    # bind to localhost:6789 for new connections
+    server.bind(("localhost", 6789))
+    
+    # listen for one client at most
+    server.listen(1)
 
-sock = socket.socket()
+    # blockingly wait for a new client
+    client, _ = server.accept()
+    
+    # blockingly waut for some data
+    data = client.recv(1024)
 
-request = b"HEAD / HTTP/1.0\r\nHOST: github.com\r\n\r\n"
+    # reverse the data
+    data = data[::-1]
 
-sock.connect(("github.com", 80))
+    # blockingly send the data
+    client.sendall(data)
 
-sock.sendall(request)
+    # close client and server
+    client.close()
+    server.close()
 
-response = sock.recv(1024)
-
-end = time.perf_counter()
-
-print(response.decode())
-
-print(f"The application ran in: {end-start:0.2f}s")
-
+if __name__ == "__main__":
+    # blockingly run the server
+    server()
 
