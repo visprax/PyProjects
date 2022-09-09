7802484ecbcfb24ad3c25b96dce0049bf1b735cd
Saeid Aliei <saeidaliei2019@gmail.com>
2022-09-09 13:15:21 +0430

    add async run_in_executor examples

---
+++ 28-crawler/supplemental/04-async-executor-threads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""A simple demonstration to use threads for blocking calls with async coroutines."""
+
+import time
+import asyncio
+import concurrent.futures
+
+
+def blocking_call(n):
+    print(f"starting blocking call, param: {n}")
+    time.sleep(0.1)
+    print(f"blocking call done, w/  param: {n}")
+    return n ** 2
+
+async def threads_for_blockers(exector):
+    print("starting threaded blockers")
+    loop = asyncio.get_event_loop()
+
+    print("creating exector tasks")
+    blocking_tasks = [loop.run_in_executor(exector, blocking_call, i) for i in range(6)]
+
+    print("awaiting executor tasks")
+    completed, pending = await asyncio.wait(blocking_tasks)
+
+    results = [task.result() for task in completed]
+    print(f"results: {results}")
+
+if __name__ == "__main__":
+
+    exector = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+
+    asyncio.run(threads_for_blockers(exector))
+
+
+
