c2177a2d0df7438c8e0042a0a368e6347712be11
Saeid Aliei <saeidaliei2019@gmail.com>
2022-10-22 23:30:00 +0330

    add marketplace microservice

---
+++ 22-microservices/marketplace/marketplace.py
@@ -0,0 +1,20 @@
+import os
+import grpc
+from flask import Flask, render_template
+
+from recommendations_pb2 import BookCategory, RecommendationRequest
+from recommendations_pb2_grpc import RecommendationStub
+
+recommendations_host    = os.getenv("RECOMMENDATIONS_HOST", "localhost")
+recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
+recommendations_client  = RecommendationStub(recommendations_channel)
+
+@app.route('/')
+def homepage():
+    recommendations_request  = RecommendationRequest(user_id=1, category=BookCategory.SCI_FI, max_results=3)
+    recommendations_response = recommendations_client.Recommend(recommendations_request)
+
+    return render_template("homepage.html", recommendations=recommendations_response.recommendations)
+
+
+
