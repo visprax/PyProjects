4b2a48aa5d70f92e9cc15ff0ca98b997d978efe6
Saeid Aliei <saeidaliei2019@gmail.com>
2022-10-22 23:58:36 +0330

    add flask app name

--- 22-microservices/marketplace/marketplace.py
+++ 22-microservices/marketplace/marketplace.py
@@ -3,11 +3,14 @@ import grpc
 from flask import Flask, render_template
 
 from recommendations_pb2 import BookCategory, RecommendationRequest
-from recommendations_pb2_grpc import RecommendationStub
+from recommendations_pb2_grpc import RecommendationsStub
+
+app = Flask(__name__)
 
 recommendations_host    = os.getenv("RECOMMENDATIONS_HOST", "localhost")
 recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
-recommendations_client  = RecommendationStub(recommendations_channel)
+recommendations_client  = RecommendationsStub(recommendations_channel)
+
 
 @app.route('/')
 def homepage():
