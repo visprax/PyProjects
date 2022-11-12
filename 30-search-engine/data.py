6f302d8a7e96160a75ba2bbb2df810f41b641824
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-12 19:39:31 +0330

    handle data gathering

--- 30-search-engine/data.py
+++ 30-search-engine/data.py
@@ -3,10 +3,21 @@
 import csv
 import ast
 from pathlib import Path
+from datetime import datetime
 
 
 plot_summary_filepath   = Path("data/MovieSummaries/plot_summaries.txt")
 movie_metadata_filepath = Path("data/MovieSummaries/movie.metadata.tsv")
+
+def datetime_handler(date_string):
+    try:
+        dt = datetime.strptime(date_string, "%Y-%m-%d")
+    except ValueError:
+        try:
+            dt = datetime.strptime(date_string, "%Y")
+        except Exception as err:
+            dt = None
+    return dt
 
 if not plot_summary_filepath.exists():
     # download and warn
@@ -14,16 +25,24 @@ if not plot_summary_filepath.exists():
 
 with plot_summary_filepath.open() as plot_file:
     reader = csv.reader(plot_file, delimiter="\t")
-    # a list of movie id and summary tuples, (id, summary)
-    summaries = [(line[0], line[1]) for line in reader]
+    # a list of movie id and summary tuples: (id, summary)
+    summaries = [(int(line[0]), line[1]) for line in reader]
 
     
 with movie_metadata_filepath.open() as metadata_file:
     reader = csv.reader(metadata_file, delimiter="\t")
-    # a list of movie metadata tuples,
-    # (id, name, release_data, box_office_revenue, runtime, languages, countries, genres)
-    metadata = [(line[0], line[2], line[3], line[4], line[5], 
-            list(ast.literal_eval(line[6])), 
-            list(ast.literal_eval(line[7])),
-            list(ast.literal_eval(line[8])) 
-        )]
+    # a list of movie metadata tuples:
+    # (id, name, release_date, box_office_revenue, runtime, languages, countries, genres)
+    metadata = [(int(line[0]), line[2], 
+            # if release_date is empty, make it None, otherwise try to convert it to datetime object
+            (lambda line: None if line[3] == '' else datetime_handler(line[3]))(line),
+            # if revenue or runtime is empty string, instead of converting to float, make it None
+            (lambda line: None if line[4] == '' else float(line[4]))(line), 
+            (lambda line: None if line[5] == '' else float(line[5]))(line), 
+            list(ast.literal_eval(line[6]).values()), 
+            list(ast.literal_eval(line[7]).values()),
+            list(ast.literal_eval(line[8]).values()) 
+        ) for line in reader]
+
+print(summaries[:2])
+print(metadata[:2])
