2b23640cb6cdfb90534a662b2f607e0ef7bbdab3
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-12 22:14:32 +0330

    add lazy load functionality

--- 30-search-engine/data.py
+++ 30-search-engine/data.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
-
+import os
 import csv
 import ast
+import time
+import logging
+import resource
+import rich.progress
 from pathlib import Path
 from datetime import datetime
 
 
 plot_summary_filepath   = Path("data/MovieSummaries/plot_summaries.txt")
 movie_metadata_filepath = Path("data/MovieSummaries/movie.metadata.tsv")
+
+plot_summary_filesize   = os.stat(plot_summary_filepath).st_size
+movie_metadata_filesize = os.stat(movie_metadata_filepath).st_size
+
 
 def datetime_handler(date_string):
     try:
@@ -23,13 +30,17 @@ if not plot_summary_filepath.exists():
     # download and warn
     pass
 
-with plot_summary_filepath.open() as plot_file:
+start = time.time()
+
+# with plot_summary_filepath.open() as plot_file:
+with rich.progress.open(plot_summary_filepath) as plot_file:
     reader = csv.reader(plot_file, delimiter="\t")
     # a list of movie id and summary tuples: (id, summary)
     summaries = [(int(line[0]), line[1]) for line in reader]
 
     
-with movie_metadata_filepath.open() as metadata_file:
+# with movie_metadata_filepath.open() as metadata_file:
+with rich.progress.open(movie_metadata_filepath) as metadata_file:
     reader = csv.reader(metadata_file, delimiter="\t")
     # a list of movie metadata tuples:
     # (id, name, release_date, box_office_revenue, runtime, languages, countries, genres)
@@ -44,5 +55,50 @@ with movie_metadata_filepath.open() as metadata_file:
             list(ast.literal_eval(line[8]).values()) 
         ) for line in reader]
 
-print(summaries[:2])
-print(metadata[:2])
+end = time.time()
+print(f"main execution time: {end-start:.2f}s.")
+
+# print(summaries[:5])
+# print(metadata[:2])
+
+def get_line(filepath):
+    with open(filepath, 'r') as f:
+        for line in f:
+            yield line
+
+def process_lines(lines, delimiter="\t"):
+    for line in lines:
+        record = line.strip().split(delimiter)
+        yield record
+
+def get_records(filepath):
+    line = get_line(filepath)
+    yield from process_lines(line)
+
+
+start = time.time()
+r = get_records(plot_summary_filepath)
+end = time.time()
+for _ in range(42306):
+    print(f"{_}")
+    next(r)
+print(f"yield execution time: {end-start:.2f}s.")
+
+
+# print these in logging
+print('Peak Memory Usage =', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+print('User Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_utime)
+print('System Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_stime)
+
+class DataHandler:
+    def __init__(self, filepath, progress_bar=False, record_timings=True):
+        self.filepath = filepath
+        self.progress_bar = progress_bar
+        self.record_timings = record_timings
+
+    def process_lines(self):
+        summaries = [(int(line[0]), line[1]) for line in self.reader]
+
+
+class LazyDataHandler:
+
