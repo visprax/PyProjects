61c3a6f082b3038db2209a9fff2fb9ce9569cf51
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-18 23:03:06 +0330

    utf-8 encode response

--- 24-web-server/utils/tcp.py
+++ 24-web-server/utils/tcp.py
@@ -9,20 +9,20 @@ logger = logging.getLogger("tcp")
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     """Handles HTTP requests."""
 
+    page = """\
+    <html>
+    <body>
+        <p> Hello Web Servers! </p><br>
+    </body>
+    </html>
+    """
+
     def do_GET(self):
         """Overwrites base method to handle GET requests."""
         logger.debug("handling GET request")
+        encoded_page = self.page.encode("utf-8")
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(page.encode("utf-8"))))
+        self.send_header("Content-Length", str(len(encoded_page)))
         self.end_headers()
-
-        page = f"""\
-        <html>
-        <body>
-            <p> Hello Web Servers! </p><br>
-            <p> URL: {self.path} </p>
-        </body>
-        </html>
-        """
-        self.wfile.write(self.page)
+        self.wfile.write(encoded_page)
