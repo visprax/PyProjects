cd782902c0354cc7431766c016e34fea938bb1c8
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-18 23:47:48 +0330

    add extra info to reponse body

--- 24-web-server/utils/tcp.py
+++ 24-web-server/utils/tcp.py
@@ -12,17 +12,39 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     page = """\
     <html>
     <body>
-        <p> Hello Web Servers! </p><br>
+            <p> Hello Web Servers! </p><br>
+        <table>  
+            <tr>  <td>Header</td>        <td>Value</td>          </tr>
+            <tr>  <td>Date & Time:</td>  <td>{date_time}</td>    </tr>
+            <tr>  <td>Client Host:</td>  <td>{client_host}</td>  </tr>
+            <tr>  <td>Client Port:</td>  <td>{client_port}</td>  </tr>
+            <tr>  <td>Command:</td>      <td>{command}</td>      </tr>
+            <tr>  <td>Path:</td>         <td>{path}</td>         </tr>
+        </table>
     </body>
     </html>
     """
 
     def do_GET(self):
-        """Overwrites base method to handle GET requests."""
+        """Overwrites the base method to handle GET requests."""
         logger.debug("handling GET request")
-        encoded_page = self.page.encode("utf-8")
+        body = self.response_body()
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(encoded_page)))
+        self.send_header("Content-Length", str(len(body)))
         self.end_headers()
-        self.wfile.write(encoded_page)
+        self.wfile.write(body)
+
+    def response_body(self):
+        """Creates the response body to be sent by the server."""
+        logger.debug("creating response body")
+        vals = {
+                "date_time"   : self.date_time_string(),
+                "client_host" : self.client_address[0],
+                "client_port" : self.client_address[1],
+                "command"     : self.command,
+                "path"        : self.path
+                }
+        resp = self.page.format(**vals).encode("utf-8")
+        return resp
+
