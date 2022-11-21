f31fbec1a9d41a1609eab2ec876da2c69027aa1b
Saeid Aliei <saeidaliei2019@gmail.com>
2022-11-21 22:32:57 +0330

    add in-disk file serving

--- 24-web-server/utils/tcp.py
+++ 24-web-server/utils/tcp.py
@@ -1,13 +1,14 @@
 
 """A simple TCP/IP server implementation."""
 
+import os
 import logging
 import http.server
 
 logger = logging.getLogger("tcp")
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-    """Handles HTTP requests."""
+    """Handle HTTP requests."""
 
     page = """\
     <html>
@@ -25,19 +26,57 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     </html>
     """
 
-    def do_GET(self):
-        """Overwrites the base method to handle GET requests."""
-        logger.debug("handling GET request")
-        body = self.response_body()
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(body)))
-        self.end_headers()
-        self.wfile.write(body)
+    error_page = """\
+    <html>
+    <body>
+        <h3> Access Error! </h3>
+        <p>Error while accessing: {path}</p>
+        <p>{err}</p>
+    </html>
+    </body>
+    """
 
-    def response_body(self):
-        """Creates the response body to be sent by the server."""
-        logger.debug("creating response body")
+    def do_GET(self):
+        """Overwrite the base method to handle GET requests."""
+        logger.debug("handling GET request")
+        try:
+            abs_path = os.getcwd() + self.path
+            if not os.path.exists(abs_path):
+                raise RuntimeError(f"'{abs_path}': not found")
+            elif os.path.isfile(abs_path):
+                self.handle_file(abs_path)
+            else:
+                raise NotImplementedError("'{abs_path}': unknown object")
+        except Exception as err:
+            self.handle_error(err)
+
+    def handle_file(self, path):
+        try:
+            # TODO: avoid reading whole file into memory!
+            with open(path, 'rb') as f:
+                content = f.read()
+            self.send_content(content)
+        except IOError as err:
+            self.handle_error(err)
+
+    def handle_error(self, err):
+        """Handle error page and error log."""
+        logger.error(f"exception occured during handling GET request", exc_info=True)
+        content = self.error_page.format(path=self.path, err=err).encode("utf-8")
+        self.send_content(content, 404)
+
+    def send_content(self, content, status=200):
+        """Handle response creation and sending."""
+        logger.debug("creating response and sending it")
+        self.send_response(status)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def _dynamic_response(self):
+        """Respond with a static page, generated on the fly."""
+        logger.debug("dynamically responding with an on-the-fly generated page")
         vals = {
                 "date_time"   : self.date_time_string(),
                 "client_host" : self.client_address[0],
@@ -45,6 +84,5 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                 "command"     : self.command,
                 "path"        : self.path
                 }
-        resp = self.page.format(**vals).encode("utf-8")
-        return resp
-
+        content = self.page.format(**vals).encode("utf-8")
+        self.send_content(content, 200)
