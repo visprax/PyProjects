a0b9af7586ebaed68cb7a73bedef8d37681524ed
saliei <saeid.aliei@gmail.com>
2022-12-20 20:05:59 +0330

    change current peer to node

--- 16-P2P-network/utils/peer.py
+++ 16-P2P-network/utils/peer.py
@@ -1,4 +1,4 @@
-"""Peer module. Node specific functinalities of the P2P network."""
+"""Peer module. Peer specific functinalities in the P2P network."""
 
 import socket
 import logging
@@ -9,44 +9,64 @@ logger = logging.getLogger("peer")
 class Peer:
     """Implements the core functionalities of a peer in the network."""
 
-    def __init__(self, maxpeers, peerport, peerhost = None, peerid = None):
-        """Initialize a peer that keeps info about maxpeers number of peers, 
-        listening on peerport with the id of peerid at the peerhost address.
-        If a host address is not given, one will be given by trying to connect
-        to a public internet host, e.g. Google."""
+    def __init__(self, maxpeers, nodeport, nodehost = None, nodeid = None):
+        """Initialize a node. 
 
-        logger.info(f"initializing peer:  'maxpeers': {maxpeers}, 'port': {peerport}, 'host': {peerhost}, 'id': {peerid}")
+        The node, which is the current peer, keeps info about maxpeers number 
+        of other peers, listening on nodeport with the id of nodeid at the nodehost 
+        address. If a host address is not given, one will be given by trying to connect 
+        to a public internet host, e.g. Google.
+        """
+
+        logger.info(f"initializing node:  'maxpeers': {maxpeers}, \
+                'port': {nodeport}, 'host': {nodehost}, 'id': {nodeid}")
 
         self.maxpeers = maxpeers
-        self.peerport = peerport
+        self.nodeport = nodeport
 
-        if peerhost:
-            self.peerhost = peerhost
+        if nodehost:
+            self.nodehost = nodehost
         else:
-            self.__inithost()
+            self._inithost()
 
-        if peerid:
-            self.peerid = peerid
+        if nodeid:
+            self.nodeid = nodeid
         else:
-            self.__initid()
+            self._initid()
 
         self.peerlock = threading.Lock() #? or threading.RLock()?
 
         # known peers to this node
         self.peers = {} #?
         self.handlers = {}
-        # used to break the main peer loop
+        # used to break the main node loop
         self.breakloop = False
         self.router = None #?
 
+    def getsocket(self, port, backlog = 5):
+        """Prepare a socket for the node to listen on the given port."""
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # make socket reusable
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # optional backlog param is the number of non-accepted connections allowed to be queued up
+        s.listen(backlog)
+        return s
 
-    def __inithost(self):
-        """If no peerhost were given, determine one by connecting to a public online host."""
-        pass
+    def _inithost(self):
+        """If no nodehost were given, determine one by connecting to a public online host."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect(("www.google.com", 80))
+                self.nodehost = s.getsockname()[0]
+                logger.info(f"node host address set to: {self.peerhost}")
+            except Exception as err:
+                logger.error("node host address couldn't be determined", exc_info=True)
 
-    def __initid(self):
-        """If no peerid were given, set it to 'peerhost:peerport'"""
-        self.peerid = f"{self.peerhost}:{self.peerport}"
+    def _initid(self):
+        """If no nodeid were given, set it to 'host:port'"""
+        _id = f"{self.nodehost}:{self.nodeport}"
+        self.nodeid = _id
+        logger.debug(f"nodeid set to {_id}")
 
 
 
