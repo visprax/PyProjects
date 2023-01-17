ebaed63604b65ce5a80fb14f11c00c25a059bc4e
Saeid Aliei <saeid.aliei@gmail.com>
2023-01-18 01:39:13 +0330

    add reduced matrix function

--- 19-LLVM/c/linear_solver_runtime.c
+++ 19-LLVM/c/linear_solver_runtime.c
@@ -1,23 +1,43 @@
 /*
- * Solve a system of linear equations using Cramer's rule.
+ * Solve a system of linear equations (ax = b) using Cramer's rule.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// the matrix of coefficients is of size NxN
 #define N 4
 
-double det(double *a)
+// -1 to the power of x
+int minus_one_power(int x)
 {
-    if (N == 1) return a[0];
+    if((x % 2) == 0) return 1;
+
+    return -1;
+}
+
+void reduce_a(double* a, double* ra, size_t n, size_t i)
+{
+        for(size_t j = 0; j < n-1; j++)
+        {
+            ra[(n-1)*i + j] = a[n*i + j];
+        }
+}
+
+// the matrix 'a' is of size nxn
+double det(double* a, size_t n)
+{
+    if (n == 1) return a[0];
 
     double d = 0.0;
-    for(int i = 0; i < N*N; i++)
+    for(size_t i = 0; i < n; i++)
     {
-        double buf[N*N]; 
-        memcpy(buf, a, sizeof(buf));
+       // the reduced matrix of 'a' with row 0 and column i removed
+       double ra[(n-1) * (n-1)]; 
+       reduce_a(a, ra, n, i);
 
+       d += a[i] * minus_one_power(i) * det(ra, n-1);
     }
     
     return d;
@@ -34,6 +54,7 @@ int main()
     double b[N] = {-3, -32, -47, 49};
     double x[N];
 
-    double d = det(a);
-    /*printf("d: %f\n", d);*/
+    double d = det(a, sizeof(a) / sizeof(a[0]));
+    printf("d: %f\n", d);
+
 }
